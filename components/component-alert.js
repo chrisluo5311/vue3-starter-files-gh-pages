8d16f759f3d1cb58e2f7f0335660acf8186a4272
chrisluo5311 <chrislo5311@gmail.com>
2023-08-02 16:41:08 +0800

    init

---
+++ components/component-alert.js
@@ -0,0 +1,10 @@
+export default {
+    data() {
+        return {
+          text: '外部匯入'
+        };
+      },
+      template: `<div class="alert alert-primary" role="alert">
+        {{ text }}
+      </div>`
+}
